0d97f6c31878ea104b28d140b42861a27370eeb5
ElkinContrerasR <escontreras-2023a@corhuila.edu.co>
2025-05-04 07:02:55 -0500

    cambio en el controlador

--- demo/src/main/java/com/ejemplo/demo/HelloController.java
+++ demo/src/main/java/com/ejemplo/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
   @GetMapping("/")
   public String hello() {
-    return " Spring Boot!";
+    return " Hola desde Spring Boot!";
   }
 }
