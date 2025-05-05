430f304893b518dad3bdc00622c3f5e46f2a9d30
ElkinContrerasR <escontreras-2023a@corhuila.edu.co>
2025-05-05 15:07:11 -0500

    cambio

--- demo/src/main/java/com/ejemplo/demo/HelloController.java
+++ demo/src/main/java/com/ejemplo/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
   @GetMapping("/")
   public String hello() {
-    return " Hola desde Spring Boot!";
+    return " Hola elkin";
   }
 }
